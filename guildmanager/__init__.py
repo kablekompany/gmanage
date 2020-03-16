26c67b5e33095d854ad8d2c2323df2a39107f9be
dragdev <samueljohnnicholson@gmail.com>
2020-03-16 00:44:08 +0000

    fix loading cog (potentially)

--- guildmanager/__init__.py
+++ guildmanager/__init__.py
@@ -1,2 +1,7 @@
 __version__ = 1
 from guildmanager.cog import GMcog as GuildManager
+from guildmanager.cog import setup as cogsetup
+
+def setup(bot):
+	"""Alias for cogsetup() when doing bot.load_extension"""
+	cogsetup(bot)
