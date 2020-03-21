b1098ff1d1ec6afab5370242fa076ea53062ce1e
dragdev <samueljohnnicholson@gmail.com>
2020-03-21 12:05:20 +0000

    bump version

--- guildmanager/__init__.py
+++ guildmanager/__init__.py
@@ -15,7 +15,7 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER I
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-__version__ = "0.3.6"
+__version__ = "0.3.7"
 from guildmanager.cog import GMcog as GuildManager
 from guildmanager.cog import setup as _setup
 
